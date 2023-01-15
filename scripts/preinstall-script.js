2137776df2ee5b20ce635daa0cd3c2ac33e081f4
Rachit Dua <rachit2427@gmail.com>
2023-01-15 23:06:35 +0200

    Added React Navigation package (#5)

---
+++ scripts/preinstall-script.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+
+/**
+ * Do NOT allow using `npm` as package manager.
+ */
+if (process.env.npm_execpath.indexOf('yarn') === -1) {
+  console.error('You must use Yarn to install dependencies:');
+  console.error('$ yarn install');
+
+  process.exit(1);
+}
