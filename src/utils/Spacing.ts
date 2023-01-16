2843b5403f50fb7740289bc5c9150540f4da2f76
Rachit Dua <rachit2427@gmail.com>
2023-01-16 04:50:51 +0200

    Added Spacing

---
+++ src/utils/Spacing.ts
@@ -0,0 +1,18 @@
+export const Spacing = {
+  /** 2 */
+  'xx-small': 2,
+  /** 4 */
+  'x-small': 4,
+  /** 8 */
+  small: 8,
+  /** 12 */
+  medium: 12,
+  /** 16 */
+  large: 16,
+  /** 20 */
+  'x-large': 20,
+  /** 24 */
+  'xx-large': 24,
+  /** 32 */
+  'xxx-large': 32,
+};
