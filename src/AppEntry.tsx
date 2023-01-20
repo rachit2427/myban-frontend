45dd74c472df1bd34d562ff5fafee8f08036f476
Rachit Dua <rachit2427@gmail.com>
2023-01-20 04:06:02 +0200

    Integrated Store

--- src/AppEntry.tsx
+++ src/AppEntry.tsx
@@ -1,9 +1,11 @@
 import React, { memo, useEffect } from 'react';
 
 import { useKeyboardShown } from '@src/hooks/useKeyboardShown';
+import { useMount } from '@src/hooks/useMount';
 import { AppNavigator } from '@src/navigation/AppNavigator';
 import { useAppDispatch } from '@src/state/hooks';
 import { keyboardHidden, keyboardShown } from '@src/state/slices/keyboard';
+import { loadIBAN } from '@src/state/thunk/iban';
 
 const AppEntryComponent: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -17,6 +19,10 @@ const AppEntryComponent: React.FC = () => {
 
     dispatch(keyboardHidden());
   }, [dispatch, isKeyboardShown]);
+
+  useMount(() => {
+    dispatch(loadIBAN());
+  });
 
   return <AppNavigator />;
 };
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,33 +4,31 @@ import { Box } from 'react-native-flex-layout';
 import { ScrollView } from '@src/components/ScrollView';
 import { Text } from '@src/components/Text';
 import { useTheme } from '@src/hooks/useTheme';
+import { useAppSelector } from '@src/state/hooks';
+import { selectIBANs } from '@src/state/slices/iban';
+import { Spacing } from '@src/utils/Spacing';
 
-interface HomeProps {}
-
-const HomeComponent: React.FC<HomeProps> = ({}) => {
+const HomeComponent: React.FC = () => {
+  const ibans = useAppSelector(selectIBANs);
   const { colors: themeColors } = useTheme();
 
   return (
     <ScrollView>
-      <Box bg={themeColors.backgroundPrimary}>
-        <Text type="light" color="primary500">
-          Hello
-        </Text>
-        <Text type="regular" color="secondary400">
-          Hello
-        </Text>
-        <Text type="medium">Hello</Text>
-      </Box>
-
-      <Box bg={themeColors.backgroundSecondary}>
-        <Text type="light" color="primary500">
-          Hello
-        </Text>
-        <Text type="regular" color="secondary400">
-          Hello
-        </Text>
-        <Text type="medium">Hello</Text>
-      </Box>
+      {ibans.map(iban => (
+        <Box
+          key={iban.id}
+          bg={themeColors.backgroundPrimary}
+          mt={Spacing.large}
+        >
+          <Text type="light" color="primary500">
+            IBAN: {iban.iban}
+          </Text>
+          <Text type="regular" color="secondary400">
+            First Name: {iban.firstname}
+          </Text>
+          <Text type="medium">Last Name: {iban.lastname}</Text>
+        </Box>
+      ))}
     </ScrollView>
   );
 };
