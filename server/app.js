2169f3f198758ccc147c81fc6b5895b6e547ae7f
Rachit Dua <rachit2427@gmail.com>
2023-02-06 01:33:20 +0200

    Added deployment code

--- server/app.js
+++ server/app.js
@@ -2,20 +2,22 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/version', (req, res) => {
+app.get('/version', (_, res) => {
   return res.json({
     ios: { soft: '0.0.1', hard: '0.0.0' },
     android: { soft: '0.0.1', hard: '0.0.0' },
   });
 });
 
-app.get('/*', (req, res) => {
+app.get('/*', (_, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(port);
+});
